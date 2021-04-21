0d503adeea7f968d45b6d701c509b995fde571f9
vinicius4g <viniciusdias1701@hotmail.com>
2021-04-21 19:55:09 -0300

    aula03

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { Welcome } from '../pages/Welcome'
 import { Confirmation } from '../pages/Confirmation'
 import { UserIdentification } from '../pages/UserIdentification'
+import { PlantSelect } from '../pages/PlantSelect'
 
 import colors from '../styles/colors'
 
@@ -18,7 +19,6 @@ const AppRoutes: React.FC = () => (
             }
         }}
     >
-
         <stackRoutes.Screen 
             name="Welcome"
             component={Welcome}
@@ -32,6 +32,11 @@ const AppRoutes: React.FC = () => (
         <stackRoutes.Screen 
             name="Confirmation"
             component={Confirmation}
+        />
+
+        <stackRoutes.Screen 
+            name="PlantSelect"
+            component={PlantSelect}
         />
 
 
